211f5c593d9ac366a63af79ea6b53f3fd2331b16
D7703_07 <d7703_07@lulzsec.local>
2019-04-10 16:51:31 +0900

    .

--- Single AlertController/ViewController.swift
+++ Single AlertController/ViewController.swift
@@ -15,6 +15,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: "title", message: "massage", preferredStyle: .actionSheet)
+        
+        let camAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+        
+        let libAction = UIAlertAction(title: "Library", style: .default, handler: nil)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let delAction = UIAlertAction(title: "Delete", style: .default, handler: {(action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.blue
+        })
+        
+        alertController.addAction(camAction)
+        alertController.addAction(cancelAction)
+        alertController.addAction(libAction)
+        alertController.addAction(saveAction)
+        alertController.addAction(delAction)
+        
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
+        
+        
+    }
+    
+        
+   
 }
 
